8449f69a60d38a497c0bfac05f386981b86456c3
Makai Post <postmakai@gmail.com>
2021-01-19 18:08:57 -0800

    added roughts

---
+++ routes.js
@@ -0,0 +1,28 @@
+var path = require("path");
+var UserProfile = require("./user-profiles");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        if (UserProfile.active == null) {
+            res.sendFile(path.join(__dirname, "./public/login.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "./public/profile.html"));
+        }
+    });
+    
+    app.get("/profile", function(req, res) {
+        if (UserProfile.active == null) {
+            res.sendFile(path.join(__dirname, "./public/login.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "./public/profile.html"));
+        }
+    });
+    
+    app.get("/book", function(req, res) {
+        if (UserProfile.active == null) {
+            res.sendFile(path.join(__dirname, "./public/login.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "./public/book.html"));
+        }
+    });
+}
