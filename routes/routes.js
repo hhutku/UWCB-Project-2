a31fc9f147fc1876ab731c5b11e861ee762540d9
Makai Post <postmakai@gmail.com>
2021-01-20 19:41:57 -0800

    updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,28 +1,43 @@
 var path = require("path");
-var UserProfile = require("../user-profiles");
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        if (UserProfile.active == null) {
-            res.sendFile(path.join(__dirname, "./public/login.html"));
+        if (!req.user) {
+            res.sendFile(path.join(__dirname, "../public/login.html"));
         } else {
-            res.sendFile(path.join(__dirname, "./public/profile.html"));
+            res.sendFile(path.join(__dirname, "../public/profile.html"));
+        }
+    });
+
+    app.get("/login", function(req, res) {
+        if (!req.user) {
+            res.sendFile(path.join(__dirname, "../public/login.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "../public/profile.html"));
+        }
+    });
+
+    app.get("/signup", function(req, res) {
+        if (!req.user) {
+            res.sendFile(path.join(__dirname, "../public/signup.html"));
+        } else {
+            res.sendFile(path.join(__dirname, "../public/profile.html"));
         }
     });
     
     app.get("/profile", function(req, res) {
-        if (UserProfile.active == null) {
-            res.sendFile(path.join(__dirname, "./public/login.html"));
+        if (!req.user) {
+            res.sendFile(path.join(__dirname, "../public/login.html"));
         } else {
-            res.sendFile(path.join(__dirname, "./public/profile.html"));
+            res.sendFile(path.join(__dirname, "../public/profile.html"));
         }
     });
     
     app.get("/book", function(req, res) {
-        if (UserProfile.active == null) {
-            res.sendFile(path.join(__dirname, "./public/login.html"));
+        if (!req.user) {
+            res.sendFile(path.join(__dirname, "../public/login.html"));
         } else {
-            res.sendFile(path.join(__dirname, "./public/book.html"));
+            res.sendFile(path.join(__dirname, "../public/book.html"));
         }
     });
 }
