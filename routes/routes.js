7766afa5a7e8fc38d49f9a9a3caace0d137d357a
Makai Post <postmakai@gmail.com>
2021-01-29 22:02:23 -0800

    pull from main

--- routes/routes.js
+++ routes/routes.js
@@ -14,37 +14,17 @@ module.exports = function(app) {
     res.render("login");
   });
 
-<<<<<<< HEAD
-    app.get("/signup", function (req, res) {
-        res.render("signUp");
-    });
-
-    app.get("/profile", isAuthenticated, function (req, res) {
-        res.render("userProfile");
-    });
-=======
   app.get("/", (req, res) => {
     res.render("login");
   });
->>>>>>> 47bb6b12d7b2e8b1d400f41e432e3794e9fb4b62
 
   app.get("/signup", (req, res) => {
     res.render("signUp");
   });
 
-<<<<<<< HEAD
-        for (let i = 0; i < bookId.length; i++) {
-            if (selected === bookId[i].routeName) {
-                return res.json(bookId[i]);
-            }
-        }
-        res.render("bookPage");
-    });
-=======
   app.get("/profile", isAuthenticated, (req, res) => {
     res.render("userProfile");
   });
->>>>>>> 47bb6b12d7b2e8b1d400f41e432e3794e9fb4b62
 
   app.get("/book", (req, res) => {
     res.render("bookPage");
