911fc93d88122f7a271d0803b0c1244cd69a9368
Elizabeth Brandt <ebrandt8@uw.edu>
2021-01-30 02:36:27 -0800

    fixed linting

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-
 // require('dotenv').config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("./config/passport");
 const PORT = process.env.PORT || 8080;
-const db = require("./models")
+const db = require("./models");
 
 const app = express();
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,15 +13,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use(
-    // session({ secret:process.env.SESSION_SECRET, resave: true, saveUninitialized: true })
-    session({ secret:"asflkjd", resave: true, saveUninitialized: true })
+  // session({ secret:process.env.SESSION_SECRET, resave: true, saveUninitialized: true })
+  session({ secret: "asflkjd", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
 const apiRoutes = require("./routes/post-api-routes");
 app.use(apiRoutes);
-
 
 require("./routes/routes")(app);
 
@@ -34,13 +30,10 @@ app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 //end handlebars stuff
 
-const routes = require("./routes/routes");
-
 // const socket = require("socket.io");
 
-db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-        console.log("Server listening on: http://localhost:" + PORT);
-    })
-})
-
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+});
